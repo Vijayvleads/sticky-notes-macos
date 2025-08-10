36b3d53ac27d133e4276ba7e97c4d44e14fb3104
Vijay kumar <bvijay2202@gmail.com>
2025-08-11 00:46:26 +0530

    Create vite.config.ts

---
+++ vite.config.ts
@@ -0,0 +1,26 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  root: resolve(__dirname, 'src/renderer'),
+  base: './',
+  server: {
+    port: 5173,
+    strictPort: true
+  },
+  build: {
+    outDir: resolve(__dirname, 'dist/renderer'),
+    emptyOutDir: true,
+    rollupOptions: {
+      input: resolve(__dirname, 'src/renderer/index.html')
+    }
+  },
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src/renderer')
+    }
+  }
+})
